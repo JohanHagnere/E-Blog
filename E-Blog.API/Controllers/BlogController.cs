bf54428248e5ab88f0a47301a16566ccddeac279
Johan Hagnere <johan.hagnere@gmail.com>
2023-10-17 13:42:31 +0200

    Ajoutez des fichiers projet.

---
+++ E-Blog.API/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Blog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet(Name = "hello-world")]
+        public IActionResult GetHelloWorld()
+        {
+            string htmlContent = "<h1>Hello world!</h1>";
+            return Content(htmlContent, "text/html");
+        }
+    }
+}
---
+++ E-Blog.Test/BlogController.Test.cs
@@ -0,0 +1,10 @@
+namespace E_Blog.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestGetString()
+        {
+        }
+    }
+}
